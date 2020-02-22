53bb1a1bfd235604dc273aa40d6dcd86a090bc02
Youngkun <youngkunzhu@163.com>
2020-02-22 15:51:07 +0800

    完成登录退出功能

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,12 @@
 <template>
       <Layout>
-            <Sider hide-trigger></Sider>
-            <Layout>
-                <Header></Header>
+            <Sider id="sider" hide-trigger></Sider>
+            <Layout id="insider-wrapper">
+                <Header id="header">
+                    <top-nav></top-nav>
+                </Header>
                 <Content>
-                    主页
+                    <p>主页</p>
                 </Content>
                 <Footer></Footer>
             </Layout>
@@ -12,11 +14,37 @@
 </template>
 
 <script>
+    import TopNav from "@/views/TopNav";
     export default {
-        name: "Index"
+        name: "Index",
+        components: {
+            TopNav,
+        }
     }
 </script>
 
 <style scoped>
+    #sider {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        z-index: 1000;
+    }
 
+    #insider-wrapper {
+        min-height: 100vh;
+        padding-left: 200px;
+        padding-top: 64px;
+    }
+
+    #header {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: calc(100% - 200px);
+        background: #fff;
+        z-index: 1000;
+        box-shadow: 0 0 6px 1px rgba(53, 64, 82, 0.1);
+    }
 </style>
---
+++ src/views/TopNav.vue
@@ -0,0 +1,68 @@
+<template>
+    <Row type="flex">
+        <!--左边菜单-->
+        <Menu mode="horizontal" theme="light" class="menu">
+            <MenuItem name="1">
+                <Icon type="ios-paper"/>
+                <span>左侧菜单</span>
+            </MenuItem>
+        </Menu>
+        <!--右边菜单-->
+        <Dropdown trigger="click" v-if="username">
+        <span>
+            {{ username }}
+            <Icon type="ios-arrow-down"></Icon>
+        </span>
+            <DropdownMenu slot="list">
+                <DropdownItem @click.native="handleLogout">退出</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+    </Row>
+</template>
+
+<script>
+    import {mapGetters, mapActions} from 'vuex'
+    import cookie from "@/store/cookie";
+
+    export default {
+        name: "TopNav",
+        computed: {
+            ...mapGetters([
+                'username'
+            ])
+        },
+        methods: {
+            ...mapActions([
+                'setUser'
+            ]),
+            handleLogout() {
+                cookie.delCookie('username');
+                cookie.delCookie('token');
+                Promise.resolve(this.setUser()).then(() => {
+                    this.$router.go(0);
+                    this.$Message.success('退出成功');
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .menu {
+        height: 64px;
+        line-height: 64px;
+        margin-right: auto;
+    }
+
+    .ivu-menu-horizontal.ivu-menu-light:after {
+        height: 0;
+    }
+
+    .ivu-dropdown {
+        cursor: pointer;
+    }
+
+    .ivu-dropdown:hover {
+        color: #2d8cf0;
+    }
+</style>
