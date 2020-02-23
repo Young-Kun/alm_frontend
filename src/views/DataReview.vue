d82e5f4f73159ac0b9254d6b5924cbf04f404453
Youngkun <youngkunzhu@163.com>
2020-02-23 13:55:59 +0800

    增加分页功能

--- src/views/DataReview.vue
+++ src/views/DataReview.vue
@@ -1,7 +1,11 @@
 <template>
     <Row :gutter="24">
+        <BackTop :height="1"></BackTop>
         <i-col style="padding-right: 188px">
             <data-item-card v-for="item in data_list" :key="item.id" :data_item="item"></data-item-card>
+            <Card dis-hover :bordered="false" style="text-align: right">
+                <Page show-total :page-size="filterParams.page_size" :total="data_count" @on-change="handlePagination"/>
+            </Card>
         </i-col>
         <Card style="position: fixed; right: 24px; top: 88px; width: 150px">
             <h2 slot="title">
@@ -27,22 +31,30 @@
         data() {
             return {
                 data_list: [],
+                data_count: 0,
                 filter_years: null,
                 filterParams: {
-                    file_name__icontains: null
+                    file_name__icontains: null,
+                    page: null,
+                    page_size: 6
                 }
             }
         },
         methods: {
             getDataList(params) {
                 return this.$api.data.datatList(params).then((response) => {
-                    this.data_list = response.data;
+                    this.data_list = response.data.results;
+                    this.data_count = response.data.count;
                 }).catch(error => {
                     console.log(error.response);
                 })
             },
             handleFilterYear(name) {
                 this.filterParams.file_name__icontains = name === 'all' ? null : name;
+                this.getDataList(this.filterParams);
+            },
+            handlePagination(page) {
+                this.filterParams.page = page;
                 this.getDataList(this.filterParams);
             }
         },
