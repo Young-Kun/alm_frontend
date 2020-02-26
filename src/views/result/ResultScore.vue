958b02106323d84ce4e74556fd82b6c10adc7a2c
Youngkun <youngkunzhu@163.com>
2020-02-26 17:25:43 +0800

    完成前端页面

--- src/views/result/ResultScore.vue
+++ src/views/result/ResultScore.vue
@@ -1,20 +1,43 @@
 <template>
     <div>
-        <Card>
-            <DatePicker ref="monthStart"
-                        type="month"
-                        :options="monthStartOptions"
-                        @on-change="handleMonthStartChange"
-                        placeholder="选择起始月">
-            </DatePicker>
-            到
-            <DatePicker ref="monthEnd"
-                        type="month"
-                        :options="monthEndOptions"
-                        @on-change="handleMonthEndChange"
-                        placeholder="选择结束月">
-            </DatePicker>
-        </Card>
+        <Row :gutter="24">
+        <i-col :span="12">
+            <Card>
+                <p slot="title"> 请选择起止月份 </p>
+                <DatePicker ref="monthStart"
+                            type="month"
+                            :options="monthStartOptions"
+                            v-model="monthStart"
+                            @on-change="handleMonthStartChange"
+                            placeholder="选择起始月">
+                </DatePicker>
+                到
+                <DatePicker ref="monthEnd"
+                            type="month"
+                            :options="monthEndOptions"
+                            v-model="monthEnd"
+                            @on-change="handleMonthEndChange"
+                            placeholder="选择结束月">
+                </DatePicker>
+            </Card>
+        </i-col>
+        <i-col :span="12">
+            <Card>
+                <p slot="title"> 请选择指标项（可多选） </p>
+                <Button type="text" slot="extra" @click="clearSelected">清空</Button>
+                <Select multiple v-model="selectedIndicators">
+                    <Option v-for="(indicator, idx) in indicators" :key="idx" :value="indicator.value">
+                        {{ indicator.label }}
+                    </Option>
+                </Select>
+            </Card>
+        </i-col>
+    </Row>
+        <Row style="margin-top: 12px">
+            <Card>
+
+            </Card>
+        </Row>
     </div>
 </template>
 
@@ -24,7 +47,30 @@
         data() {
             return {
                 monthStartOptions: null,
-                monthEndOptions: null
+                monthEndOptions: null,
+                monthEnd: null,
+                monthStart: null,
+                selectedIndicators: null,
+                indicators: [
+                    {value: 'tot_score', label: '总分'},
+                    {value: 'dur_score', label: '期限结构匹配得分'},
+                    {value: 'cost_return_score', label: '成本收益匹配得分'},
+                    {value: 'cash_flow_score', label: '现金流匹配得分'},
+                    {value: 'dur_gap_l_scaled', label: '规模调整后的修正久期缺口得分'},
+                    {value: 'dur_gap_a_scaled', label: '资产调整后的期限缺口得分'},
+                    {value: 'hedge_rate', label: '利率风险对冲率得分'},
+                    {value: 'dv', label: '基点价值变动率得分'},
+                    {value: 'avg_3y_gap', label: '三年平均年化综合投资收益率与寿险业务三年平均负债资金成本率差额得分'},
+                    {value: 'comp_gap', label: '年化综合投资收益率与寿险业务负债资金成本率差额得分'},
+                    {value: 'ra_comp_gap', label: '风险调整后的年化综合投资收益率与寿险业务负债保证成本率差额得分'},
+                    {value: 'fin_gap', label: '年化会计投资收益率与寿险业务负债有效成本率差额'},
+                    {value: 'gap_stress1', label: '压力情景一下预计差额得分'},
+                    {value: 'gap_stress2', label: '压力情景二下预计差额得分'},
+                    {value: 'gap_stress3', label: '压力情景三下预计差额得分'},
+                    {value: 'cash_flow_test_base', label: '现金流测试得分'},
+                    {value: 'cash_flow_test_stress', label: '现金流压力测试得分'},
+                    {value: 'liquidity', label: '流动性指标得分'},
+                ]
             }
         },
         methods: {
@@ -43,6 +89,9 @@
                         return date > start;
                     }
                 }
+            },
+            clearSelected() {
+                this.selectedIndicators = null;
             }
         }
     }
