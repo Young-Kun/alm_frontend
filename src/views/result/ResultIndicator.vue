0fdc730a185ca500c58ab06fdc794b3e14256d31
Youngkun <youngkunzhu@163.com>
2020-02-29 13:21:23 +0800

    完成久期

--- src/views/result/ResultIndicator.vue
+++ src/views/result/ResultIndicator.vue
@@ -158,10 +158,11 @@
                             opt.grid = {left: 60};
                             opt.yAxis = {type: 'category'};
                             opt.xAxis = {show: false};
+                            opt.legend = {bottom: 0, data: ['会计准备金', '期末资金运用净额']};
                             opt.series = [];
                             opt.series.push(
-                                {type: 'line', data: reserve_acc, name: '会计准备金', symbol: 'emptyDiamond', symbolSize:8},
-                                {type: 'line', encode: {x: 'tot'}, name: '期末资金运用净额', symbolSize:8},
+                                {type: 'line', data: reserve_acc, name: '会计准备金', symbol: 'emptyDiamond'},
+                                {type: 'line', encode: {x: 'tot'}, name: '期末资金运用净额'},
                                 {type: 'bar', encode: {x: 'cash'}, name: '现金及流动性管理工具', stack: acc, barWidth: '36%'},
                                 {type: 'bar', encode: {x: 'fixed_income'}, name: '固定收益类投资资产', stack: acc},
                                 {type: 'bar', encode: {x: 'equity'}, name: '权益类投资资产', stack: acc},
@@ -190,8 +191,8 @@
                         opt.series.push(
                             {type: 'bar', encode: {y: 'in_scaled'}, name: '规模调整后的现金流入修正久期'},
                             {type: 'bar', encode: {y: 'l_out'}, name: '负债现金流出修正久期', barGap: 0, barCategoryGap: '50%'},
-                            {type: 'line', encode: {y: 'gap_l_scaled'}, name: '规模调整后的修正久期缺口', symbolSize:8},
-                            {type: 'line', encode: {y: 'a'}, name: '资产修正久期', symbol: 'emptyDiamond', symbolSize:8},
+                            {type: 'line', encode: {y: 'gap_l_scaled'}, name: '规模调整后的修正久期缺口'},
+                            {type: 'line', encode: {y: 'a'}, name: '资产修正久期', symbol: 'emptyDiamond'},
                         )
                     })
                 }).catch(error => {
