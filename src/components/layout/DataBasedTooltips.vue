d5bf763307f12a868c056c32bb6580a656667472
Youngkun <youngkunzhu@163.com>
2020-03-01 16:05:18 +0800

    完成样式

--- src/components/layout/DataBasedTooltips.vue
+++ src/components/layout/DataBasedTooltips.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="tooltips-wrapper">
-        <Tooltip placement="right-start" v-for="menu in menuListData" :key="menu.id" class="tooltip">
-            <i :class="['fa fa-fw '] + [menu.icon]"></i>
-            <div slot="content">
-
-            </div>
-        </Tooltip>
+    <div class="wrapper">
+        <div v-for="menu in menuListData" :key="menu.id">
+            <Tooltip placement="right" class="item" :content="menu.span" v-if="!menu.menu_items">
+                <i :class="['fa fa-fw '] + [menu.icon]"></i>
+            </Tooltip>
+            <Dropdown v-if="menu.menu_items" class="item" placement="right-start">
+                <span><i :class="['fa fa-fw '] + [menu.icon]"></i></span>
+                <DropdownMenu slot="list" style="margin-left: 1px; margin-top: -22px">
+                    <DropdownItem v-for="item in menu.menu_items" :key="item.id">{{item.span}}</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </div>
     </div>
 </template>
 
@@ -22,20 +27,37 @@
 </script>
 
 <style scoped>
-    .tooltips-wrapper {
+    .wrapper {
         height: calc(100vh - 64px);
         overflow-y: auto;
     }
 
-    .tooltip {
-        color: #fff;
+    .item {
+        display: block;
         width: 100%;
+        color: rgba(255, 255, 255, 0.7);
         padding: 14px 0;
         text-align: center;
         cursor: pointer;
     }
 
-    .tooltip:hover {
+    .item:hover {
         background: #101117;
+        color: #fff;
+    }
+    
+    .item ul {
+        background: #191a23;
+        border-radius: 5px;
+        padding: 5px 0;
+    }
+
+    .item li {
+        color: rgba(255, 255, 255, 0.7);
+    }
+
+    .item li:hover {
+        background: #101117;
+        color: #fff;
     }
 </style>
