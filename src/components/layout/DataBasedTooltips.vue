1d9849459f0a809c869973fec0d89452a5542deb
Youngkun <youngkunzhu@163.com>
2020-03-01 15:26:12 +0800

    完成图标

--- src/components/layout/DataBasedTooltips.vue
+++ src/components/layout/DataBasedTooltips.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="tooltips-wrapper">
-        <Tooltip placement="right-start" v-for="menu in menuListData" :key="menu.id">
+        <Tooltip placement="right-start" v-for="menu in menuListData" :key="menu.id" class="tooltip">
+            <i :class="['fa fa-fw '] + [menu.icon]"></i>
+            <div slot="content">
 
+            </div>
         </Tooltip>
     </div>
 </template>
@@ -22,5 +25,17 @@
     .tooltips-wrapper {
         height: calc(100vh - 64px);
         overflow-y: auto;
+    }
+
+    .tooltip {
+        color: #fff;
+        width: 100%;
+        padding: 14px 0;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    .tooltip:hover {
+        background: #101117;
     }
 </style>
