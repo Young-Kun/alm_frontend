70e56b5274b6c80f47dbf4a3c2ea6ccbbabb1e07
Youngkun <youngkunzhu@163.com>
2020-03-02 20:32:13 +0800

    完成单页

--- src/utils/html2Pdf.js
+++ src/utils/html2Pdf.js
@@ -5,6 +5,8 @@ import JsPDF from "jspdf";
 export default {
     install(Vue) {
         Vue.prototype.$getPdf = (id, filename) => {
+            const pageWidth = 1920;
+            const pageHeight = 1080;
             html2canvas(document.querySelector(`#${id}`), {
                 allowTaint: true,
                 taintTest: false,
@@ -12,11 +14,8 @@ export default {
                 dpi: window.devicePixelRatio * 4, //将分辨率提高到特定的DPI 提高四倍
                 scale: 4 //按比例增加分辨率
             }).then((canvas) => {
-                    let pageWidth = canvas.width;
-                    let pageHeight = pageWidth / 16 * 9;
-                    let pageData = canvas.toDataURL('image/jpeg', 1.0);
-                    let PDF = new JsPDF('l', 'pt', [pageWidth, pageHeight]);
-                    PDF.addImage(pageData, 'JPEG', 0, 0, pageWidth, pageHeight);
+                    let PDF = new JsPDF('l', 'px', [pageWidth, pageHeight]);
+                    PDF.addImage(canvas, 'gif', 0, 0, pageWidth, pageHeight);
                     PDF.save(filename + '.pdf')
                 }
             )
